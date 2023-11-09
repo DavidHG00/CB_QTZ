bb5388daf9f5b201e50d094278c83552968d2593
DavidHG00 <141274768+DavidHG00@users.noreply.github.com>
2023-11-09 15:30:29 -0600

    Add files via upload

---
+++ FCB_QTZ/src/app/EquiposComponent/EquiposComponent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-EquiposComponent',
+  templateUrl: './EquiposComponent.component.html',
+  styleUrls: ['./EquiposComponent.component.css']
+})
+export class EquiposComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- FCB_QTZ/src/app/app.module.ts
+++ FCB_QTZ/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Componentes
+import { SharedModule } from './shared/shared.module';
+import { ServiciosComponentComponent } from './ServiciosComponent/ServiciosComponent.component';
+import { EquiposComponentComponent } from './EquiposComponent/EquiposComponent.component';
+import { SoftwareComponentComponent } from './SoftwareComponent/SoftwareComponent.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [				
+    AppComponent,
+      ServiciosComponentComponent,
+      EquiposComponentComponent,
+      SoftwareComponentComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]

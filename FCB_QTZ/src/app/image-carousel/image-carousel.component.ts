08cb6ff9faaf5732a9874f268c64b29ccafcece4
DavidHG00 <141274768+DavidHG00@users.noreply.github.com>
2023-11-10 12:01:32 -0600

    FQTZ_10112023

--- FCB_QTZ/src/app/image-carousel/image-carousel.component.ts
+++ FCB_QTZ/src/app/image-carousel/image-carousel.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
 
 @Component({
   selector: 'app-image-carousel',
   templateUrl: './image-carousel.component.html',
   styleUrls: ['./image-carousel.component.css']
 })
-export class ImageCarouselComponent implements OnInit {
+export class ImageCarouselComponent implements OnInit, OnDestroy {
+  private elemento: HTMLElement | null = null;
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
+    this.elemento = document.querySelector('.mat-mdc-tab-body-content.ng-tns-c737557735-1.ng-trigger.ng-trigger-translateTab') as HTMLElement;
+    if (this.elemento) {
+      this.elemento.style.overflow = 'hidden';
+    }
   }
 
+  ngOnDestroy(): void {
+    if (this.elemento) {
+      this.elemento.style.overflow = ''; // Restaura el comportamiento predeterminado al salir del componente
+    }
+  }
 }
